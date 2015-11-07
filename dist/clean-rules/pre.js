34390b46ff4d1dcb47072879a9467fbfc830e93d
lwdos <1127622940@qq.com>
2015-11-07 17:13:36 +0800

    clean

--- dist/clean-rules/pre.js
+++
@@ -1,7 +0,0 @@
-
-_cmd_define('/clean-rules/pre.js', function(_cmd_require, exports, module) {
-module.exports = function(code) {
-    code = code.replace(/<\/?(code|p|span|i|em)[^>]*>/gi, '');
-    return '<code>' + code.replace(/^\s*[\n\r]+/, '').replace(/[\r\n]+\s*$/, '') + '</code>';
-};
-});
