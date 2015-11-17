3f24f98f747412ba2c241b825a84a001b2622d5f
lwdos <1127622940@qq.com>
2015-11-17 13:24:51 +0800

    add print url auto fix

--- src/editor/umeditor/umeditor.js
+++ src/editor/umeditor/umeditor.js
@@ -7162,8 +7162,10 @@ UM.commands['preview'] = {
                 '<script src="' + path + 'third-party/jquery.min.js"></script>' +
                 '<script src="' + path + 'third-party/mathquill/mathquill.min.js"></script>':'';
             title = '<title>' + document.getElementById('title').value + '</title>';
+            w.name = document.getElementById('href').value;
 
         formula += '<link rel="stylesheet" href="editor.css">';
+        formula += '<script>location.hash=window.name;</script>';//将window.name写入hash
         d.open();
         d.write('<html><head>' + formula + title + '</head><body class="editor preview"><div>'+c+'</div></body></html>');
         d.close();
